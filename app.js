b5dfdc88441e5f0ce0765bd27c1d617b88ea8192
Ford Henley <fordp86@gmail.com>
2023-08-17 16:12:07 -0500

    completed random user project
    Completed random user vue.js project

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const app = Vue.createApp({
       const res = await fetch("https://randomuser.me/api/");
       const { results } = await res.json();
 
-      console.log(results);
-      this.firstName = "Sam";
-      this.lastName = "Smith";
-      this.email = "sam@gmail.com";
-      this.gender = "female";
-      this.picture = "https://randomuser.me/api/portraits/women/10.jpg";
+      //   console.log(results);
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.large;
     },
   },
 });
